e4f39984133e9c267bfd90675e85a862b4cf1e43
ti1anium <markhomepost@gmail.com>
2026-06-05 11:14:30 +0300

    .

---
+++ script.js
@@ -0,0 +1,31 @@
+const main = document.getElementById('main');
+const list = document.getElementById('list');
+
+(async () => {
+    let currentPage;
+
+    const data = await (await fetch("/subjects/subjects-data.json")).json();
+    const pages = data.pages;
+
+    pages.forEach(v => {
+        const element = document.createElement('a');
+
+        element.innerText = v;
+
+        element.onclick = async e => {
+            e.preventDefault();
+            
+            if (currentPage == v)
+                return;
+
+            currentPage = v;
+
+            const data = await (await fetch("/subjects/" + v + ".html")).text();
+            
+            main.innerHTML = data;
+        }
+        
+        list.appendChild(element);
+    });
+
+})();
